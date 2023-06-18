088c7dcfa4e26b05386d132ba0f1a252ca227b55
Azael R <azael.devel@gmail.com>
2023-06-18 01:01:23 -0700

    creación de repositorio

---
+++ tests/v0.cc
@@ -0,0 +1,31 @@
+
+#include "v0.hh"
+#include <0/math.hh>
+
+
+namespace numbers_here = oct::nums::v0;
+
+
+int v0_init(void)
+{
+
+
+	return 0;
+}
+int v0_clean(void)
+{
+
+
+	return 0;
+}
+
+
+void v0_developing()
+{
+
+
+}
+
+
+
+
---
+++ tests/v0.hh
@@ -0,0 +1,16 @@
+
+
+#ifndef OCTETOS_NUMBERS_V0_HH
+#define OCTETOS_NUMBERS_V0_HH
+
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+#define infimium 0.0001
+
+int v0_init(void);
+int v0_clean(void);
+
+void v0_developing();
+
+#endif
