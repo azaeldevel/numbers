01d902fc4329e8404745d70232c73a33d530062e
Azael <azael.devel@gmail.com>
2024-08-29 19:22:32 -0700

    save

--- 0/equation.hh
+++ 0/equation.hh
@@ -7,46 +7,6 @@
 namespace oct::nums::v0
 {
 
-    /**
-    *\brief Resuleve 'x' en la ecuacion de la forma ax = b
-    *
-    */
-    template<core::number T, core::number V = core::convertion_to_real<T>::type> constexpr V resolve(V const& a,V const& b)
-    {
-        return a/b;
-    }
-
-    /**
-    *\brief Resuleve 'x' en la ecuacion de la forma ax = b
-    *
-    */
-    template<core::decimal T,core::index auto S> constexpr matrix<T,S,1> resolve(matrix<T,S,S> const& a, matrix<T,S,1> const& b)
-    {
-        auto x = a.inverse() * b;
-        return x;
-    }
-
-    /**
-    *\brief Resuleve 'x' en la ecuacion de la forma ax = b
-    *
-    */
-    template<core::decimal T,core::index auto S> constexpr matrix<T,S,1> resolve(matrix<T,S,S> const& a, vector<T,S> const& b)
-    {
-        auto x = a.inverse() * b;
-        return x;
-    }
-
-
-    /**
-    *\brief Resuleve una ecuacion de la forma ax + b = y
-    *
-    */
-    template<core::number T, core::number V = core::convertion_to_real<T>::type> constexpr V resolve(V const& a,V const& b,V const& y)
-    {
-        return (y + b) / a;
-    }
-
-
 }
 
 #endif // OCTETOS_NUMBERS_MATH_HH
