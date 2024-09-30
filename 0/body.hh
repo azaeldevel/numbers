ea4f26152e99bf6dfcd8cbc7b2288524682fbceb
Azael <azael.devel@gmail.com>
2024-09-29 21:13:48 -0700

    save

---
+++ 0/body.hh
@@ -0,0 +1,19 @@
+
+#ifndef OCTETOS_NUBERS_V0_BODY_HH
+#define OCTETOS_NUBERS_V0_BODY_HH
+
+#include "vector.hh"
+
+namespace oct::nums::v0
+{
+    /**
+    *\brief
+    *
+    */
+    template<core::number C = double>
+    struct Body : core::array<vector<C>>
+    {
+    };
+}
+
+#endif
