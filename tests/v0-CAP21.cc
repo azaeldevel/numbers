c4765571500b5040514907af23b01485a3e843c6
Azael <azael.devel@gmail.com>
2024-11-17 08:20:30 -0800

    save

---
+++ tests/v0-CAP21.cc
@@ -0,0 +1,33 @@
+
+#if defined(__linux__)
+
+#elif defined(_WIN32) || defined(_WIN64)
+
+#else
+	#error "Plataforma desconocida"
+#endif
+
+
+
+
+#include "v0.hh"
+#include <numbers/0/vector.hh>
+
+
+namespace oct::nums::v0
+{
+    namespace numbers = oct::nums::v0;
+    namespace core = oct::core::v3;
+
+
+}
+
+namespace numbers = oct::nums::v0;
+
+
+void v0_FIUNSEZEKYI12_CAP21()
+{
+
+}
+
+
